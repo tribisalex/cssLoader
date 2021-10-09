fda18404b34dcfdedbddb28679e2ba9de0bdc9d9
Alexandr <tribisalexandr@yandex.ru>
2021-10-09 10:26:44 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Loader from '../loader/Loader'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Loader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/loader/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './loader.module.css'
+
+const Loader = () => {
+    <div className={s.spinner}></div>
+}
+
+export default Loader;
